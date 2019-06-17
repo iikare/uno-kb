d989613b08efcddfdf65f9b2da9bbdc4e5bbc01a
pkishi <rsdharmaji@gmail.com>
2019-06-16 22:10:54 -0700

    first working version

--- kb/kb.h
+++
@@ -1,4 +0,0 @@
-#define PIN_SPECIAL 13
-#define INPUT_F7 64
-
-void DeInitKey();
--- kb/kb.ino
+++ kb/kb.ino
@@ -1,27 +1,33 @@
-#include "kb.h"
+#define PIN_KEYSWITCH 7
+#define ITEM_F7 0x40
 
-int pinstate = 1;
-uint8_t buf[8] = { 0 };
+uint8_t buf[8] = {0};
 
 void setup(){
   Serial.begin(9600);
-  
-  pinMode(PIN_SPECIAL, INPUT);
-  digitalWrite(PIN_SPECIAL, 1);
+
+  pinMode(PIN_KEYSWITCH, INPUT);
+  digitalWrite(PIN_KEYSWITCH, 1);
 
   delay(200);
 }
 
 void loop(){
-  if(digitalRead(PIN_SPECIAL) != 1){
-    buf[2] = INPUT_F7;
+  if(digitalRead(PIN_KEYSWITCH) != 1) {
+    buf[2] = ITEM_F7;
+
     Serial.write(buf, 8);
-    DeInitKey();
+  }
+  else{
+  	endstroke();
   }
 }
 
-void DeInitKey(){
+void endstroke(){
   buf[0] = 0;
   buf[2] = 0;
+
   Serial.write(buf, 8);
+
+  delay(500);
 }
