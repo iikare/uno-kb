4f728801f87a8fc57f3853ace1c83081abb13242
pkishi <rsdharmaji@gmail.com>
2019-07-04 12:40:22 -0700

    add input limiting (3x per 500ms)

--- kb/kb.ino
+++ kb/kb.ino
@@ -2,32 +2,53 @@
 #define ITEM_F7 0x40
 
 uint8_t buf[8] = {0};
+static unsigned int init_timer = 0;
+static int run_counter = 0;
+static bool timeout = false;
 
 void setup(){
-  Serial.begin(9600);
+	Serial.begin(9600);
 
-  pinMode(PIN_KEYSWITCH, INPUT);
-  digitalWrite(PIN_KEYSWITCH, 1);
+	pinMode(PIN_KEYSWITCH, INPUT);
+	digitalWrite(PIN_KEYSWITCH, 1);
 
-  delay(100);
+	delay(100);
 }
 
 void loop(){
-  if(digitalRead(PIN_KEYSWITCH) != 1) {
-    buf[2] = ITEM_F7;
+	init_timer = (millis() % 500);
 
-    Serial.write(buf, 8);
-  }
-  else{
-  	endstroke();
-  }
+	if(!timeout){
+		if(digitalRead(PIN_KEYSWITCH) != 1) {
+			run_counter++;
+			buf[2] = ITEM_F7;
+
+			Serial.write(buf, 8);
+		}
+		else{
+			endstroke();
+		}
+ 	}
+
+	if(run_counter >= 3){
+		timeout = true;
+		run_counter = 0;
+		if(buf[2] == ITEM_F7){
+			endstroke();
+		}
+	}
+
+	if(init_timer == 0){
+		timeout = false;
+	}
+
 }
 
 void endstroke(){
-  buf[0] = 0;
-  buf[2] = 0;
+	buf[0] = 0;
+	buf[2] = 0;
 
-  Serial.write(buf, 8);
+	Serial.write(buf, 8);
 
-  delay(25);
+	delay(25);
 }
